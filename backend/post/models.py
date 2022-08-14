895e010baef8512ccfcb0707f67f189d2f3da17c
KawshikBss <kawshikbss@gmail.com>
2022-08-15 01:10:42 +0600

    created Post model

--- backend/post/models.py
+++ backend/post/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user.models import Profile
+import uuid
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    author = models.ForeignKey(Profile, null=True, on_delete=models.SET_NULL)
+    description = models.TextField(max_length=500, blank=True, null=True)
+    image_link = models.CharField(max_length=200, null=True, blank=True)
+    up_votes = models.IntegerField(default=0)
+    down_votes = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
