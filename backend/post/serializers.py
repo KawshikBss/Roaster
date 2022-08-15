905abb928f50e0b5f37d08b55382e59e0a093b66
KawshikBss <kawshikbss@gmail.com>
2022-08-15 15:04:27 +0600

    created serializer for comments model

--- backend/post/serializers.py
+++ backend/post/serializers.py
@@ -1,10 +1,15 @@
 from dataclasses import field
 from pyexpat import model
 from rest_framework import serializers
-from .models import Post
+from .models import Post, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
         fields = '__all__'
