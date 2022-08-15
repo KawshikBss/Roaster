ec78c6c9de816cd95d16ce9b1e7831c20959329e
KawshikBss <kawshikbss@gmail.com>
2022-08-15 15:08:49 +0600

    created api routes for comments

--- backend/post/urls.py
+++ backend/post/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('post/<str:pk>', views.getSinglePost, name='SinglePost'),
     path('add-post/', views.addPost, name='AddPost'),
     path('delete-post/<str:pk>', views.deletePost, name='DeletePost'),
+    
+    path('comments/', views.getAllComments, name='AllComments'),
+    path('comment/<str:pk>', views.getSingleComment, name='SingleComment'),
+    path('add-comment/', views.addComment, name='AddComment'),
+    path('delete-comment/<str:pk>', views.deleteComment, name='DeleteComment'),
 ]
